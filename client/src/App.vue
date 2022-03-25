761e66e9c175ced4fddf59e2c7c311ef65e975fb
chenjiahau <ivan.chen@dlinkcorp.com>
2022-03-25 09:40:57 +0800

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Sendessage></Sendessage>
+  </div>
+</template>
+
+<script>
+import Sendessage from './components/SendMessage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Sendessage
+  }
+}
+</script>
+
+<style>
+#app {
+  margin-top: 20px;
+}
+</style>
---
+++ client/src/components/SendMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Vue</h1>
+</template>
+
+<script>
+export default {
+  name: 'SendMessage'
+}
+</script>
+
+<style scoped>
+</style>
