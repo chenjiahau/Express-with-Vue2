205d9297aab4f626b91fbd501309795715e85318
chenjiahau <ivan.chen@dlinkcorp.com>
2022-04-06 15:38:21 +0800

    Form

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,82 @@
 <template>
   <div id="app" class="container">
-    <Sendessage></Sendessage>
+    <form @submit.prevent>
+      <!-- Input[text] -->
+      <div class="form-group">
+        <h5>Name</h5>
+        <input type="text" class="form-control" id="name" v-model="ticket.name"/>
+      </div>
+      <!-- Textarea -->
+      <div class="form-group">
+        <h5>Question</h5>
+        <textarea class="form-control" id="question" v-model="ticket.question"></textarea>
+      </div>
+      <!-- Input[radio] -->
+      <div class="form-group">
+        <h5>Feeling</h5>
+        <div v-for="(feeling, index) in feelingList" :key="index">
+          <input type="radio" :id="feeling.id" :value="feeling" v-model="ticket.feeling" />
+          <label for="feeling">{{feeling.title}}</label>
+        </div>
+      </div>
+      <!-- Select -->
+      <div class="form-group">
+        <h5>Product</h5>
+        <select style="width: 100%" v-model="ticket.product">
+          <option v-for="(product, index) in productList" :key="index" :value="product">{{product.title}}</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary" @click="send()">Send</button>
+      </div>
+    </form>
+    <div class="ticket">
+      <h1 class="header">Ticket</h1>
+      <div class="title">Name</div>
+      <div>{{ticket.name}}</div>
+      <hr />
+      <div class="title">Question</div>
+      <div>{{ticket.question}}</div>
+      <hr />
+      <div class="title">Feeling</div>
+      <div>{{ticket.feeling.title}}</div>
+      <hr />
+      <div class="title">Product</div>
+      <div>{{ticket.product.title}}</div>
+      <hr />
+    </div>
   </div>
 </template>
 
 <script>
-import Sendessage from './components/SendMessage.vue'
-
 export default {
   name: 'App',
-  components: {
-    Sendessage
+  data: function() {
+    return {
+      ticket: {
+        name: null,
+        question: null,
+        product: null,
+        feeling: null,  
+      },
+      feelingList: [
+        { id: 1, title: "Happy" },
+        { id: 2, title: "Angry" }
+      ],
+      productList: [
+        { id: 1, title: "Phone" },
+        { id: 2, title: "Notebook" },
+      ]
+    }
+  },
+  created() {
+    this.ticket.feeling = this.feelingList[0];
+    this.ticket.product = this.productList[0];
+  },
+  methods: {
+    send: function() {
+      console.log('send');
+    }
   }
 }
 </script>
@@ -18,5 +84,21 @@ export default {
 <style>
 #app {
   margin-top: 20px;
+}
+
+.form-group {
+  margin-bottom: 16px;
+}
+
+.ticket {
+  margin-top: 22px;
+}
+
+.ticket > .header {
+  margin-bottom: 12px;
+}
+
+.ticket > .title {
+  font-weight: bold;
 }
 </style>
