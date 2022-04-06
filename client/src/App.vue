7e2e29436fb79ba54c054ec80af289c7fc7340ba
chenjiahau <ivan.chen@dlinkcorp.com>
2022-04-06 14:58:09 +0800

    Add a new car

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,124 @@
 <template>
   <div id="app" class="container">
-    <Sendessage></Sendessage>
+    <div class="block">
+      <button
+        class="btn"
+        @click="openAdd = !openAdd;addCar()"
+        :class="{'btn-primary': !openAdd, 'btn-danger': openAdd}"
+      >
+        {{buttonName}}
+      </button>
+    </div>
+    <div class="block" v-show="openAdd">
+      <div class="form-group block">
+        <h5>Car</h5>
+        <input type="text" class="form-control" id="car" v-model="newOne.car"/>
+      </div>
+      <div class="form-group block">
+        <h5>Color</h5>
+        <input type="text" class="form-control" id="color" v-model="newOne.color"/>
+      </div>
+      <div class="form-group block">
+        <h5>Popular</h5>
+        <div>
+          <input type="radio" :value="true" v-model="newOne.popular" />
+          <label>Yes</label>
+        </div>
+        <div>
+          <input type="radio" :value="false" v-model="newOne.popular" />
+          <label>No</label>
+        </div>
+      </div>
+    </div>
+    <div class="block">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Color</th>
+            <th scope="col">Car</th>
+            <th scope="col">Popular</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(car, index) in carList" :key="car.id">
+            <th scope="row">{{index + 1}}</th>
+            <td>{{field('color', car)}}</td>
+            <td>{{field('car', car)}}</td>
+            <td>{{field('popular', car)}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import Sendessage from './components/SendMessage.vue'
+import carList from './data/car';
 
 export default {
   name: 'App',
-  components: {
-    Sendessage
+  data: function() {
+    return {
+      openAdd: false,
+      newOne: {
+        car: null,
+        color: null,
+        popular: false
+      },
+      carList: []
+    }
+  },
+  created() {
+    this.carList = carList;
+  },
+  methods: {
+    addCar: function() {
+      if (this.openAdd)
+        return;
+
+      if (!this.newOne.car)
+        return;
+
+      this.carList.push({
+        id: this.carList.length + 1,
+        car: this.newOne.car,
+        color: this.newOne.color,
+        popular: this.newOne.popular
+      });
+
+      this.openAdd = false;
+    }
+  },
+  computed: {
+    buttonName: function() {
+      if (!this.openAdd) {
+        return 'Add';
+      } else {
+        return 'Save';
+      }
+    },
+    field: function() {
+      return (target, car) => {
+        if (target !== 'popular') {
+          return car[target];
+        } else {
+          if (car[target]) {
+            return 'Marker';
+          }
+        }
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  margin-top: 20px;
-}
+  .block {
+    margin: 12px 0;
+  }
+
+  input {
+    margin-right: 6px;
+  }
 </style>
