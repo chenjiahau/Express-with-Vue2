abec301429f3a29b9dd40fd6df81772368ee6c43
chenjiahau <chenjiahau@gmail.com>
2022-04-07 19:27:57 +0800

    Interceptor

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,10 @@ import VueResource from 'vue-resource';
 Vue.use(VueResource);
 Vue.http.options.root = 'localhost:8080';
 Vue.http.headers.common['Content-Type'] = 'application/json; charset=utf-8';
+Vue.http.interceptors.push((request, next) => {
+  console.log('interceptors');
+  next();
+});
 
 import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
